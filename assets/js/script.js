969959befc74cfa49f54b31b91928a883b552813
piaacv <122986202+piaacv@users.noreply.github.com>
2024-03-12 22:07:03 -0300

    Estructura lógica juego cachipun terminada

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,25 @@
-// let suma = 0;
-// let contador = 0;
-// for (let i = 0; i <= 50; i++) {
-//     if(i % 2 !== 0){
-//         suma += i;
-//         contador++;
-//     }
-//   }
-
-//   console.log(suma, contador)
-
-
 let ronda = Number(prompt("Ingresa cantidad de rondas"));
-let victoriasJugador = 0;
-let victoriasComputador = 0;
-let empate = 0;
-let resultado = "";
 
-for (let i = 1; i <= ronda; i++){
-    jugar(i);
-}
+for (let i = 1; i <= ronda; i++) {
+  let jugadorUsuario = prompt(
+    "Ingresa tu elección: piedra, papel o tijera"
+  ).toLocaleLowerCase();
+  let jugada = ["piedra", "papel", "tijera"];
+  let indiceJugadorComputador = Math.floor(Math.random() * jugada.length);
+  let jugadorComputador = jugada[indiceJugadorComputador];
+  document.write(
+    `Ronda ${i}<br>Resultado Jugador: ${jugadorUsuario}<br>Resultado Computadora: ${jugadorComputador} <br>`
+  );
 
-resultado += victoriasJugador
-
-function jugar(){
-    let papel = 0;
-    let tijera = 1;
-    let roca = 2;
-
+  if (jugadorUsuario === jugadorComputador) {
+    alert("Empate");
+  } else if (
+    (jugadorUsuario === "piedra") & (jugadorComputador === "tijera") ||
+    (jugadorUsuario === "papel") & (jugadorComputador === "piedra") ||
+    (jugadorUsuario === "tijera") & (jugadorComputador === "papel")
+  ) {
+     alert("Ganaste");
+  } else {
+    alert("Ganó la computadora, sigue jugando");
+  }
 }
